9aef364104685e4e850de043ce853fb6ed44d18c
maltom <tomaszpmalachowski@gmail.com>
2021-10-07 00:29:26 +0200

    comms header added

--- CommunicationCodes.h
+++ CommunicationCodes.h
@@ -10,11 +10,14 @@ enum Command
     EMERGENCY_STOP_ALL_THRUSTERS = 1,
     EMERGENCY_STOP_SENDING_DATA,
 
-    /* messages where the response is required */
+    /* messages where the response is required*/
     RESPREQ_SEND_BACK_HEALTH_CHECK = 20,
 
+    /* messages which are respones by themselves */
+    RESP_HEALTH_CHECK = 60,
+
     /* messages where the response is not required */
-    NORESP_START_SENDING_SENSOR_VALUES = 100
+    NORESPREQ_START_SENDING_SENSOR_VALUES = 100
 };
 
 #endif
